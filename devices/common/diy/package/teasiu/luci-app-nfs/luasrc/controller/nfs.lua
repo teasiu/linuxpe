453a77508901db3f0a9230c0298d292fb9bd9195
teasiu <teasiu@qq.com>
2021-03-27 02:26:31 +0800

    init

--- devices/common/diy/package/teasiu/luci-app-nfs/luasrc/controller/nfs.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.nfs", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/nfs") then
-		return
-	end
-	entry({"admin", "services", "nfs"}, cbi("nfs"), _("NFS Manage"), 101).dependent = true
-end
--- dl/README.md
+++
@@ -1,5 +0,0 @@
-# myopenwrt
-
-#### 介绍
-openwrt newifi dragino2 x86_64
-
